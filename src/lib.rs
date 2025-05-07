79787b15130fe34de3facaaec5b6b4508c9eb921
Yarn <yarnnd@gmail.com>
2025-05-07 03:36:43 -0700

    cleanup

--- src/lib.rs
+++ src/lib.rs
@@ -1,21 +1,9 @@
 
 #![recursion_limit="1024"]
-// #![feature(async_await, futures_api, pin)]
-// #![feature(impl_trait_in_bindings)]
+#![allow(non_local_definitions)] // silence warning originating from failure macros
 
-// extern crate futures;
-// extern crate futures01;
-
-// extern crate hyper;
-// extern crate hyper_tls;
-// extern crate tokio;
-// extern crate tokio_tungstenite;
 #[macro_use] extern crate failure;
-// extern crate bytes;
-
-// extern crate serde;
 #[macro_use] extern crate serde_derive;
-// extern crate serde_json;
 
 pub mod gateway;
 pub mod gateway_ws;
@@ -29,9 +17,3 @@ pub use self::outer_wrapper::SendHandle;
 pub use discord_api::channel::Channel;
 
 pub use self::gateway_ws::jank_run;
-
-// pub use futures;
-// pub use tokio;
-// pub use hyper;
-// pub use serde_json;
-// pub use bytes;
